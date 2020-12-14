f1d0c76f05747b2dea3da4854c2ec5e54a44857a
Brian <briand.nester@gmail.com>
2020-12-13 19:36:01 -0500

    push

--- src/Components/Name.jsx
+++ src/Components/Name.jsx
@@ -4,6 +4,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link, animateScroll as scroll } from "react-scroll";
 import './Name.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -41,8 +42,16 @@ export default function Name() {
                         <LinkedInIcon style={{ fontSize: '40px' }} className={classes.icon}/>  
                     </a>
 
-                    
+                    <Link
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    >
                     <EmailIcon style={{ fontSize: '40px' }} className={classes.icon}/>
+                    </Link>
                     
                 </div>
             </div>
