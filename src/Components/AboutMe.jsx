5efde276597e7d60ed48f6065a3d1afd0d44beb6
Brian <briand.nester@gmail.com>
2020-12-13 19:14:42 -0500

    push

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,13 +5,19 @@ export default function AboutMe() {
     return (
         <div className='about-me-div'>
             <h2 className='about-me' id='about-me'>About Me</h2>
-            <p className='about-me-para'>words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words 
-            words words words words words words words words words words words words words words words .
+            <p className='about-me-para'>I am a full-stack Software Developer from Baltimore, Maryland.
+                                        Whether it is front-end, back-end, logic, or styling. I love
+                                        pushing myself to solve problems and grow as a developer. Throughout
+                                        my whole life, I've always found passion in projects that allows
+                                        me to use my technically and creative mind.
+                                        <br/>
+                                        <br/>
+                                        In my free time, I'm always making music. I love to play guitar,
+                                         but drums are my passion and my entire life outside of work.
+                                         <br/>
+                                         <br/>
+                                         Let's make something awesome together!
+
             </p>
         </div>
     )
--- src/Components/Name.jsx
+++ src/Components/Name.jsx
@@ -4,6 +4,7 @@ import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link, animateScroll as scroll } from "react-scroll";
 import './Name.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -27,7 +28,7 @@ export default function Name() {
         <div className='name-container'>
             <div className='names-div'>
             <   h1 className='names'>Brian <br/> Nester</h1>
-                <h2 className='subname'>Software Engineer</h2>
+                <h2 className='subname'>Software Developer</h2>
             </div>
             <div className='pic-info'>
                 <img className='port-img' src={ProfileImg} alt='portimg' height='255px' weight='255px' />
@@ -41,8 +42,16 @@ export default function Name() {
                         <LinkedInIcon style={{ fontSize: '40px' }} className={classes.icon}/>  
                     </a>
 
-                    
-                    <EmailIcon style={{ fontSize: '40px' }} className={classes.icon}/>
+                    <Link
+                    activeClass="active"
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                    >
+                        <EmailIcon style={{ fontSize: '40px' }} className={classes.icon}/>
+                    </Link>
                     
                 </div>
             </div>
